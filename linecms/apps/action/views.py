793d3692f80babf9d90908ffb5b628f82ba98585
Daniel <nieltansah@gmail.com>
2018-04-22 15:21:40 +0700

    Add debugging

--- linecms/apps/action/views.py
+++ linecms/apps/action/views.py
@@ -28,16 +28,24 @@ def callback(request):
         for event in events:
             action = None
 
+            print("GET EVENT")
+
             if isinstance(event, MessageEvent):
+                print("MESSAGE EVENT")
                 if isinstance(event.message, TextMessage):
+                    print("TEXT MESSAGE")
                     action = Message.objects.filter(
                         text=event.message.text).first()
             elif isinstance(event, PostbackEvent):
+                print("POSTBACK EVENT")
                 action = Postback.objects.filter(
                     data=event.postback.data).first()
 
             if action is not None:
+                print("ACTION")
                 action.handle_event(event, line_bot_api)
+
+            print("DONE EVENT")
 
         return HttpResponse()
     else:
