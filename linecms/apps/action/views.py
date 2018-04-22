cdb4ee2aa296a33e27cfa6ac0e4164f57810ef5b
Daniel <nieltansah@gmail.com>
2018-04-22 14:55:07 +0700

    action: views: Add LINE webhook

--- linecms/apps/action/views.py
+++ linecms/apps/action/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.conf import settings
+from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseForbidden
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+from linebot import LineBotApi, WebhookParser
+from linebot.exceptions import InvalidSignatureError, LineBotApiError
+from linebot.models import MessageEvent, PostbackEvent, TextMessage
+
+from .models import Message, Postback
+
+line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
+parser = WebhookParser(settings.LINE_CHANNEL_SECRET)
+
+
+@csrf_exempt
+def callback(request):
+    if request.method == 'POST':
+        signature = request.META['HTTP_X_LINE_SIGNATURE']
+        body = request.body.decode('utf-8')
+
+        try:
+            events = parser.parse(body, signature)
+        except InvalidSignatureError:
+            return HttpResponseForbidden()
+        except LineBotApiError:
+            return HttpResponseBadRequest()
+
+        for event in events:
+            action = None
+
+            if isinstance(event, MessageEvent):
+                if isinstance(event.message, TextMessage):
+                    action = Message.objects.filter(
+                        text=event.message.text).first()
+            elif isinstance(event, PostbackEvent):
+                action = Postback.objects.filter(
+                    data=event.postback.data).first()
+
+            if action is not None:
+                action.handle_event(event, line_bot_api)
+
+        return HttpResponse()
+    else:
+        return HttpResponseBadRequest()
