ffbe15a6169edc3ca5fe6fcad2c75e28a703c6a2
Daniel <nieltansah@gmail.com>
2018-04-22 16:41:54 +0700

    action: admin: Add interface to InvalidMessage

--- linecms/apps/action/admin.py
+++ linecms/apps/action/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 
-from .models import Message, Postback
+from .models import Message, InvalidMessage, Postback
 
 
 class MessageAdmin(admin.ModelAdmin):
     fields = ['text', 'reply', 'reply_text']
+
+
+class InvalidMessageAdmin(admin.ModelAdmin):
+    fields = ['reply', 'reply_text']
 
 
 class PostbackAdmin(admin.ModelAdmin):
@@ -12,4 +16,5 @@ class PostbackAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Message, MessageAdmin)
+admin.site.register(InvalidMessage, InvalidMessageAdmin)
 admin.site.register(Postback, PostbackAdmin)
