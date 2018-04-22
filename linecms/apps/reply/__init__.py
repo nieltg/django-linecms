b4de12128464234fbdf8ca48d55c3edf0afbc153
Daniel <nieltansah@gmail.com>
2018-04-22 13:43:08 +0700

    reply: Add and wire Djando app. module

---
+++ linecms/apps/reply/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'linecms.apps.reply.apps.ReplyConfig'
--- linecms/settings.py
+++ linecms/settings.py
@@ -35,6 +35,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'linecms.apps.reply',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
