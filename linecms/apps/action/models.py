07d8ebb10a0ca3a7cbf49a0c7262bf5be9064449
Daniel <nieltansah@gmail.com>
2018-04-22 16:55:50 +0700

    Move AbstractAction from action -> reply

--- linecms/apps/action/models.py
+++ linecms/apps/action/models.py
@@ -1,29 +1,7 @@
 from django.db import models
 from django.utils.translation import gettext as _
 
-from linecms.apps.reply.models import Text
-
-
-class AbstractAction(models.Model):
-    """Reply action."""
-
-    class Meta:
-        abstract = True
-
-    REPLY_TEXT = 1
-    REPLY_CHOICES = ((REPLY_TEXT, _("Text")), )
-
-    reply = models.IntegerField(choices=REPLY_CHOICES)
-    reply_text = models.ForeignKey(Text, on_delete=models.PROTECT)
-
-    def handle_event(self, event, line_bot_api):
-        providers = {
-            self.REPLY_TEXT: self.reply_text,
-        }
-
-        provider = providers[self.reply]
-        line_bot_api.reply_message(event.reply_token,
-                                   provider.get_line_bot_object())
+from linecms.apps.reply.models import AbstractAction
 
 
 class Postback(AbstractAction):
--- linecms/apps/reply/models.py
+++ linecms/apps/reply/models.py
@@ -13,3 +13,25 @@ class Text(models.Model):
 
     def __str__(self):
         return self.text[:100]
+
+
+class AbstractAction(models.Model):
+    """Reply action."""
+
+    class Meta:
+        abstract = True
+
+    REPLY_TEXT = 1
+    REPLY_CHOICES = ((REPLY_TEXT, _("Text")), )
+
+    reply = models.IntegerField(choices=REPLY_CHOICES)
+    reply_text = models.ForeignKey(Text, on_delete=models.PROTECT)
+
+    def handle_event(self, event, line_bot_api):
+        providers = {
+            self.REPLY_TEXT: self.reply_text,
+        }
+
+        provider = providers[self.reply]
+        line_bot_api.reply_message(event.reply_token,
+                                   provider.get_line_bot_object())
