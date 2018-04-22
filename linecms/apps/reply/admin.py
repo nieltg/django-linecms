e2b62fad06ec007f6035a5b0a4d2cd56300b836d
Daniel <nieltansah@gmail.com>
2018-04-22 17:25:04 +0700

    reply: admin: Link Group, GroupItem

--- linecms/apps/reply/admin.py
+++ linecms/apps/reply/admin.py
@@ -1,10 +1,20 @@
 from django.contrib import admin
 
-from .models import Text
+from .models import Text, Group, GroupItem
 
 
 class TextAdmin(admin.ModelAdmin):
     fields = ['text']
+
+
+class GroupItemInline(admin.TabularInline):
+    model = GroupItem
+
+
+class GroupAdmin(admin.ModelAdmin):
+    inlines = [
+        GroupItemInline,
+    ]
 
 
 admin.site.register(Text, TextAdmin)
