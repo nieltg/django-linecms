5d68c0c69a6bc03f1194b9d55bd1b29ef712b44b
Daniel <nieltansah@gmail.com>
2018-04-30 23:07:11 +0700

    Restructure project files
    Remove .env.example file

--- linecms_web/wsgi.py
+++ linecms_web/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for linecms project.
+WSGI config for linecms_web project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "linecms.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "linecms_web.settings")
 
 application = get_wsgi_application()
