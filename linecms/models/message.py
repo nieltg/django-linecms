2d284eb71a8f7817fc67886bdba6a58b332bf992
Daniel <nieltansah@gmail.com>
2018-05-23 20:34:38 +0700

    models: Add Message model and migration

---
+++ linecms/models/message.py
@@ -0,0 +1,89 @@
+from django.db import models
+from django.utils.translation import gettext as _
+from linebot.models import TextSendMessage
+
+
+class Message(models.Model):
+    """LINE message model."""
+
+    MESSAGE_TYPE_GROUP = 0
+    MESSAGE_TYPE_TEXT = 1
+    MESSAGE_TYPE_CHOICES = (
+        (MESSAGE_TYPE_GROUP, _("Message Group")),
+        (MESSAGE_TYPE_TEXT, _("Text Message")),
+    )
+    message_type = models.IntegerField(
+        _("Message type"), choices=MESSAGE_TYPE_CHOICES)
+
+    # group
+    items = models.ManyToManyField('self', verbose_name=_("Group Items"))
+    # text
+    text = models.TextField(_("Text"), max_length=2000)
+
+    # Handlers
+
+    _handlers = {}
+
+    @classmethod
+    def handler(cls, type_id):
+        def _impl(handler_cls):
+            if type_id in cls._handlers:
+                raise ValueError(
+                    "Handler for '{0}' is already defined".format(type_id))
+            cls._handlers[type_id] = handler_cls
+
+        return _impl
+
+    def _get_handler(self):
+        try:
+            return self._handlers[self.message_type]
+        except KeyError as e:
+            raise ValueError(
+                "Invalid message type: " + self.message_type) from e
+
+    # Operations
+
+    def build_linebot_message(self):
+        return self._get_handler().build_linebot_message(self)
+
+    def __str__(self):
+        return self._get_handler().to_str(self)
+
+
+class AbstractHandler:
+    @staticmethod
+    def build_linebot_message(model):
+        raise NotImplementedError()
+
+    @staticmethod
+    def to_str(model):
+        raise NotImplementedError()
+
+
+@Message.handler(Message.MESSAGE_TYPE_GROUP)
+class GroupMessageHandler(AbstractHandler):
+    fields = ('items', )
+
+    @staticmethod
+    def build_linebot_message(model):
+        queryset = model.items.all()
+        if queryset.count() > 5:
+            raise ValueError("Group items can't be more than 5")
+        return [item.build_linebot_message() for item in queryset]
+
+    @staticmethod
+    def to_str(model):
+        return _("Group: %(id)s") % {'id': model.pk}
+
+
+@Message.handler(Message.MESSAGE_TYPE_TEXT)
+class TextMessageHandler(AbstractHandler):
+    fields = ('text', )
+
+    @staticmethod
+    def build_linebot_message(model):
+        return TextSendMessage(model.text)
+
+    @staticmethod
+    def to_str(model):
+        return _("Text: %(text)s") % {'text': model.text}
